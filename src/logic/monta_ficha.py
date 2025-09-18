2bfdd12b65d8eff7b0755ecbd1e12dfc50ec708c
Mauricio <mauricioluan2023@gmail.com>
2025-09-17 22:48:52 -0300

    feat: implementa montagem da ficha

--- src/logic/monta_ficha.py
+++ src/logic/monta_ficha.py
@@ -1,9 +1,65 @@
+import datetime
 from schemas.responses import Customer
+from schemas.ficha import Ficha
 
 
 def monta_ficha(
-    protocol: str, customer: Customer, n_terminais: int, servico_cartao: str
-):
-    razao_social = customer.data[0].name.strip()
+    protocol: int, customer: Customer, n_terminais: int, servico_cartao: str
+) -> Ficha:
+    if not customer.data:
+        raise ValueError("Campo 'data' de customer está vazio.")
 
-    return razao_social, protocol, n_terminais, servico_cartao
+    codigo_payer = customer.data[0].internal_id
+    conta, empresa, loja = codigo_payer.split("-")
+
+    tokens = token(codigo_payer, n_terminais)
+
+    custom_fields = customer.data[0].custom_fields
+    linhas = {objeto.name: objeto.value for objeto in custom_fields}
+
+    chamado = f"{protocol} - {datetime.date.today().strftime('%d/%m/%Y')}"
+    fantasia_bruto = linhas.get("Nome Fantasia")
+    nome_fantasia = fantasia_bruto.strip() if fantasia_bruto else "Nao informado"
+    razao_social = customer.data[0].name.strip().upper()
+    cnpj_bruto = linhas.get("CNPJ")
+    cnpj = cnpj_bruto.strip() if cnpj_bruto else "Nao informado"
+    endereco_bruto = f"{linhas.get("Endereco")}, {linhas.get('Numero')}"
+    endereco = endereco_bruto.strip().upper() if endereco_bruto else "Nao informado"
+    bairro_bruto = linhas.get("Bairro")
+    bairro = bairro_bruto.strip().upper() if bairro_bruto else "Nao informado"
+    cidade_bruto = f"{linhas.get('Cidade')}, {linhas.get('UF')}"
+    cidade = cidade_bruto.strip().upper() if cidade_bruto else "Nao informado"
+    contato_bruto = linhas.get("COMERCIAL - Contato")
+    contato = contato_bruto.strip().upper() if contato_bruto else "Nao informado"
+    telefone_bruto = linhas.get("COMERCIAL - Telefone")
+    telefone = telefone_bruto if telefone_bruto else "Nao informado"
+    email_bruto = linhas.get("COMERCIAL - E-mail")
+    email = email_bruto.strip() if email_bruto else "Nao informado"
+
+    ficha = Ficha(
+        chamado=chamado,
+        nome_fantasia=nome_fantasia,
+        razao_social=razao_social,
+        cnpj=cnpj,
+        endereco=endereco,
+        bairro=bairro,
+        cidade=cidade,
+        contato=contato,
+        telefone=telefone,
+        email=email,
+        account=conta,
+        company=empresa,
+        store=loja,
+        token="/".join(tokens),
+        servico_cartao=servico_cartao,
+    )
+    return ficha
+
+
+def token(codigo_payer, n_terminais) -> list[str]:
+    partes = codigo_payer.split("-")
+    tokens = []
+
+    for i in range(1, int(n_terminais) + 1):
+        tokens.append(f"{partes[1]}{partes[2]}{str(i).zfill(2)}")
+    return tokens
--- src/main.py
+++ src/main.py
@@ -1,119 +1,19 @@
-# -*- coding: utf-8 -*-
-#
-# main.py, gera_planilha.py, encontra_imagem.py, config_log.py
-# Projeto: Gerador de Fichas de Implantação
-# Autor: Mauricio Luan
-# Data de criação: 29/01/2025
-# Descrição: Gera fichas de implantação para clientes, coletando dados de um chamado e formatando-os em uma planilha.
-#
-# Copyright (c) 2025 Payer Serviços de Pagamento LTDA. Todos os direitos reservados.
-
-# import datetime
-# import os
-# from src.logic.gera_planilha import gerar_planilha_estilizada
-# from src.logic.encontra_imagem import encontrar_imagem
-# from src.logic.config_log import configura_logger
-
-# log = configura_logger("gerador-fichas-3.0")
-
-
-# def organiza_os_dados(response_chamado, response_cliente, n_terminais, sc):
-#     """
-#     Realiza o filtro das informações necessárias sobre o cliente
-#     e o chamado para repassar a função que gera a planilha
-#     """
-#     log.debug("Iniciando a formatação dos dados...")
-
-#     dados_chamado = response_chamado.json()
-#     dados_cliente = response_cliente.json()
-
-#     dicionario = {}
-
-#     # Captura os dados do cliente e salva no objeto 'dicionario'
-#     dados_loja = dados_cliente["data"][0]["custom_fields"]
-#     dados_a_capturar = [
-#         "Nome Fantasia",
-#         "CNPJ",
-#         "Endereco",
-#         "Numero",
-#         "Bairro",
-#         "Cidade",
-#         "COMERCIAL - Contato",
-#         "COMERCIAL - Telefone",
-#         "COMERCIAL - E-mail",
-#     ]
-#     for campo in dados_a_capturar:
-#         for item in dados_loja:
-#             if item["name"] == campo:
-#                 dicionario[campo] = item["value"]
-
-#     # captura numero do chamado e razão social do cliente
-#     chamado = dados_chamado["data"]["protocol"]
-#     razao_social = dados_chamado["data"]["customer"]["name"].strip()
-
-#     # captura conta empresa loja e formata para exibição na planilha
-#     conta_empresa_loja = dados_cliente["data"][0]["internal_id"]
-#     conta, empresa, loja = conta_empresa_loja.split("-")
-#     dicionario["Conta"] = f"{conta} - {razao_social}"
-#     dicionario["Empresa"] = f"{empresa} - {razao_social}"
-#     dicionario["Loja"] = f"{loja} - {dicionario['Nome Fantasia']}"
-
-#     # gera os tokens para a quantidade de terminais
-#     partes = conta_empresa_loja.split("-")
-#     tokens = []
-#     for i in range(1, int(n_terminais) + 1):
-#         tokens.append(f"{partes[1]}{partes[2]}{str(i).zfill(2)}")
-
-#     # esse objeto representa a planilha que será preenchida.
-#     # as chaves representam as celulas da primeira coluna e
-#     # os valores são os dados que serão preenchidos
-#     data = datetime.date.today()
-
-#     planilha = {
-#         "Chamado": str(chamado) + " - " + str(data.strftime("%d/%m/%Y")),
-#         "Nome Fantasia": dicionario["Nome Fantasia"],
-#         "Razão Social": razao_social,
-#         "CNPJ": dicionario.get("CNPJ", ""),
-#         "Endereco": str(dicionario.get("Endereco", "") or "").upper()
-#         + ", "
-#         + str(dicionario.get("Numero", "") or "").upper(),
-#         "Bairro": str(dicionario.get("Bairro", "")).upper(),
-#         "Cidade": str(dicionario.get("Cidade", "")).upper(),
-#         "Contato": str(dicionario.get("COMERCIAL - Contato", "")).upper(),
-#         "Telefone": dicionario.get("COMERCIAL - Telefone", ""),
-#         "E-mail": dicionario.get("COMERCIAL - E-mail", ""),
-#         "Conta": dicionario["Conta"],
-#         "Empresa": dicionario["Empresa"],
-#         "Loja": dicionario["Loja"],
-#         "Token Payer": " / ".join(tokens),
-#         "Serviço Cartão": str(sc),
-#     }
-#     log.debug(f"Dados organizados: {planilha}")
-
-#     return planilha
-
-
 # def definir_contexto_salvamento(planilha):
 #     caminho_base = f"G:\\Drives compartilhados\\FICHAS DE IMPLANTACAO"
-
 #     try:
 #         if not os.path.exists(caminho_base):
 #             caminho_base = os.path.join(os.path.expanduser("~"), "Documents")
 #             log.info(
 #                 f"Caminho do google drive nao encontrado. Salvando em: {caminho_base}\n"
 #             )
-
 #         primeira_letra = planilha["Razão Social"][0].upper()
 #         subpasta_letra = os.path.join(caminho_base, primeira_letra)
 #         os.makedirs(subpasta_letra, exist_ok=True)
 #         pasta_razao_social = os.path.join(subpasta_letra, planilha["Razão Social"])
 #         os.makedirs(pasta_razao_social, exist_ok=True)
 #         arquivo_excel = os.path.join(pasta_razao_social, f"{planilha['Loja']}.xlsx")
-
 #         caminho_imagem = encontrar_imagem("payer.png")
-
 #         return arquivo_excel, caminho_imagem, pasta_razao_social
-
 #     except Exception as e:
 #         log.exception(f"Erro ao definir contexto de salvamento: {e}")
 #         return None, None, None
@@ -123,20 +23,18 @@ from pydantic import ValidationError
 from config.load_env import load_env
 from logic.get_user_info import get_id_e_terminais, get_servico_cartao
 from logic.get_tomticket_data import get_dados_ticket, get_dados_customer
-
-# from logic.monta_ficha import monta_ficha
+from logic.monta_ficha import monta_ficha
 from schemas.responses import Ticket, Customer
 
 print(
     """
-	██████╗  █████╗ ██╗   ██╗███████╗██████╗
-	██╔══██╗██╔══██╗╚██╗ ██╔╝██╔════╝██╔══██╗
-	██████╔╝███████║ ╚████╔╝ █████╗  ██████╔╝
-	██╔═══╝ ██╔══██║  ╚██╔╝  ██╔══╝  ██╔══██╗
-	██║     ██║  ██║   ██║   ███████╗██║  ██║
-	╚═╝     ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
-	Gerador de Fichas de Implantação · 2025 · Mauricio Luan
-	"""
+██████╗  █████╗ ██╗   ██╗███████╗██████╗
+██╔══██╗██╔══██╗╚██╗ ██╔╝██╔════╝██╔══██╗
+██████╔╝███████║ ╚████╔╝ █████╗  ██████╔╝
+██╔══   ██║  ██╔╝  ██╔   ██╔═══╝ ██╔══██╗
+██║     ██║  ██║   ██║   ███████╗██║  ██║
+╚═╝     ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
+Gerador de Fichas de Implantação · 2025 · Mauricio Luan"""
 )
 
 
@@ -151,22 +49,20 @@ def main():
 
     while True:
         try:
-            # captura id do ticket, numero de terminais e servico de cartao
             ticket_id, n_terminais = get_id_e_terminais()
             servico_cartao = get_servico_cartao()
 
             ticket_response = get_dados_ticket(ticket_url, ticket_id, header)
             ticket = Ticket.model_validate(ticket_response)
 
+            protocol = ticket.data.protocol
             codigo_payer = ticket.data.customer.internal_id
 
             customer_response = get_dados_customer(customer_url, codigo_payer, header)
             customer = Customer.model_validate(customer_response)
 
-            print(ticket)
-
-            # ficha = monta_ficha(codigo_payer, customer, n_terminais, servico_cartao)
-
+            ficha = monta_ficha(protocol, customer, n_terminais, servico_cartao)
+            print(f"{ficha}")
         except ValueError as e:
             print(f"\nErro: {e}")
 
@@ -175,11 +71,6 @@ def main():
 
         except Exception as e:
             print(f"\nErro inesperado: {e}")
-
-        # log.debug("Chama organiza_os_dados()")
-        # planilha = organiza_os_dados(
-        #     response_chamado, response_cliente, n_terminais, sc
-        # )
 
         # arquivo_excel, caminho_imagem, pasta_razao_social = definir_contexto_salvamento(
         #     planilha
@@ -196,5 +87,4 @@ def main():
 
 
 if __name__ == "__main__":
-    # log.debug("Aplicacao iniciada.")
     main()
